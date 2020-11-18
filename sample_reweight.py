3d19c193d5c68852856acaf6ed6f2603281d2d1f
TachyonFTL <yifan233@utexas.edu>
2020-11-18 18:35:26 -0500

    update SR

--- sample_reweight.py
+++ sample_reweight.py
@@ -1,10 +1,12 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import normalize
 from sklearn.ensemble import GradientBoostingClassifier
+from typing import List, Dict
 import torch
 import torch.nn.functional as F
 import numpy as np
 import math
+
 
 # Not Implemented yet
 # class PipelineRFE(Pipeline):
@@ -34,12 +36,6 @@ class SampleReweight:
                               (i + 1) * (self.sample_size // self.b)) for i in range(self.b)]
 
     def _get_weight(self, l: np.array) -> np.array:
-
-        if self.C is None:
-            self.C = l
-        else:
-            self.C = np.concatenate((self.C, l), axis=1)
-
         head = math.ceil(self.C.shape[1] * 0.1)
 
         l_normed = self.norm(l)
@@ -48,12 +44,13 @@ class SampleReweight:
         # print(c_normed.shape)
 
         h_1 = - l_normed
-        h_2 = self.norm((np.mean(c_normed[:, :head], axis=1) / np.mean(c_normed[:, -head:], axis=1)).reshape(self.sample_size, -1))
+        h_2 = self.norm(
+            (np.mean(c_normed[:, :head], axis=1) / np.mean(c_normed[:, -head:], axis=1)).reshape(self.sample_size, -1))
 
         h = self.a1 * h_1 + self.a2 * h_2
+        h = h.squeeze()
 
         sample_to_bin, bins = self._get_bins(h)
-
         w = np.zeros(self.sample_size)
 
         for i in range(self.sample_size):
@@ -61,7 +58,7 @@ class SampleReweight:
 
         return w
 
-    def _get_bins(self, h: np.array) -> (dict, list):
+    def _get_bins(self, h: np.array) -> (Dict[int, int], List[np.array]):
         ranks = h.argsort()
         bins = [[] for i in range(self.b)]
 
@@ -72,12 +69,13 @@ class SampleReweight:
                 if low <= ranks[sample_idx] < high:
                     idx_to_bin[sample_idx] = bin_idx
                     bins[bin_idx].append(h[sample_idx])
+                    break
 
         bins = [np.mean(np.asarray(_bin)) for _bin in bins]
 
         return idx_to_bin, bins
 
-    def cal_sample_loss(self, model):
+    def cal_sample_loss(self, model) -> np.array:
         assert isinstance(model, GradientBoostingClassifier)
         with torch.no_grad():
             predictions = torch.tensor(model.predict_log_proba(self.X))
@@ -85,11 +83,23 @@ class SampleReweight:
             loss = self.criterion(predictions, self.y).unsqueeze(1)
         return loss.numpy()
 
-    def reweight(self, model):
+    def reweight(self, model, staged_pred: List[np.array]) -> np.array:
+        self.register_C(staged_pred)
+
         loss = self.cal_sample_loss(model)
         weight = self._get_weight(loss)
         self.k += 1
+
         return weight
+
+    def register_C(self, staged_pred: List[np.array]):
+        self.C = np.zeros((len(staged_pred), self.sample_size))
+
+        with torch.no_grad():
+            for i, pred in enumerate(staged_pred):
+                self.C[i] = self.criterion(torch.tensor(pred), self.y)
+
+        self.C = self.C.transpose()
 
     @staticmethod
     def criterion(*args, weight=None, reduction='none'):
@@ -100,5 +110,4 @@ class SampleReweight:
     def norm(array: np.array) -> np.array:
         ranks = array.argsort(axis=0) + 1
         norm = ranks / array.shape[0]
-        print(norm)
         return norm
